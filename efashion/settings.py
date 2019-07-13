0fd53260ed1f17686293abbc6a896b2c77908a5a
PRINCE BANSAL <princebansal2472gmail.com>
2019-07-13 18:17:39 +0530

    Add Url

--- efashion/settings.py
+++ efashion/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend_Panel',
+    'frontend_Panel',
 ]
 
 MIDDLEWARE = [
